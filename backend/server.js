b7b260545011f92ccce35f65b125563250592bcb
Sona P <sonapalanivelraja@gmail.com>
2026-07-06 12:16:44 +0530

    Moved server.js to backend

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const fs = require("fs");
-const User = require("./usermodel");
+const User = require("../usermodel");
 
 const app = express();
 
